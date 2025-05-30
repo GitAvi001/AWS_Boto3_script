bc50d922c9c0ed32ac7e3ffbd907df8006acaa92
GitAvi001 <avindusridharmawardhana@gmail.com>
2025-05-30 12:47:48 +0530

    users accessing script changes

---
+++ user_list_script.py
@@ -0,0 +1,13 @@
+#Automated script to listing the all users in IAM 
+
+
+import boto3
+#Automted way to access the AWS management console using boto3 library
+aws_management_console = boto3.session.Session(profile_name="default")
+
+#Moving to the IAM console
+iam_console_resource = aws_management_console.resource('iam') 
+
+#for-each loop to print all users in IAM console 
+for each_user in iam_console_resource.users.all():
+    print(each_user.name)
